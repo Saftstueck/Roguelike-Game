ab578478f4917b7334ad3309a51a26be1049baf1
Saftstueck <ycy81326@gmail.com>
2026-08-20 21:52:17 +0200

    class changes

--- classes.cs
+++ classes.cs
@@ -1,11 +1,19 @@
-# Wizard, Alchemist - Attributes: great attacking spells, making potions
-# Angels, Priest, Palladin - Attributes: healing, flying (over water)
-# Assasins, Bandits, Thiefs - Attributes: stealing gold, sneaky attack
-# Rouge
-# Archers, cowboy - Attributes: Long ranged physical attack
+# Wizard - Attributes: great attacking spells
+# Alchemist - Attributes: making potions
+
+# Angel - Attributes: healing, flying (over water)
+# Priest - Attributes: attacking buffs, "blessed" (luckier)
+# Palladin - Attributes: high defence, mid meele phys attack
+
+# Assasin - Attributes: sneak attack, bleeding. chance of critical demage if getting too close
+# Bandit - Attributes: stealing gold
+# Thief - Attributes: stealing gold, traps
+
+# Archers - Attributes: Long ranged physical attack with Bow. High range (Triple Shot)
+# Cowboy - Attributes: Shooting with guns, fast attacks, Throwing with dynamite
+
 # Necromancers - Attributes: Long ranged magic attack, spawning undeads
-# 
-# 
-# 
-# 
-# 
+
+
+
+
