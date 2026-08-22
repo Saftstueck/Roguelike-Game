05d4a8dd67796461785fefb8ba3b86b09c7e0267
Saftstueck <ycy81326@gmail.com>
2026-08-22 22:37:02 +0200

    MAGES classes

--- player.cs
+++ player.cs
@@ -1,9 +1,59 @@
-# the character
-
-public class player:
+public abstract class Character
 {
-	public void UseSpell(int input)
+	public string Name {get; protected set; }
+	public int MaxHp {get; protected set; }
+	public int Hp {get; protected set; }
+
+	public int Mana {get; protected set; }
+
+	public double PhysDamageMultiplikator {get; protected set; }
+	public double MagicDamageMultiplikator {get; protected set; }
+
+	public double PhysResistanceMultiplikator {get; protected set; }
+	public double MagicResistanceMultiplikator {get; protected set; }
+
+	public Character(string name, int maxHp, int mana)
 	{
-		command.Execute();
+		Name = name;
+		MaxHp = maxHp;
+		Hp = maxHp;
+		Mana = mana;
+	}
+}
+
+public class Wizard : Character
+{
+	public Wizard(): base ("Wizard", 70, 130)
+	{
+		PhysDamageMultiplikator = 0.9;
+		MagicDamageMultiplikator = 1.1; 
+
+		PhysResistanceMultiplikator = 0.9;
+		MagicResistanceMultiplikator = 1.1;
+	}
+}
+
+
+public class Alchemist : Character
+{
+	public Alchemist(): base ("Alchemist", 50, 200)
+	{
+		PhysDamageMultiplikator = 0.7;
+		MagicDamageMultiplikator = 1.1; 
+
+		PhysResistanceMultiplikator = 0.7;
+		MagicResistanceMultiplikator = 1.3;
+	}
+}
+
+public class Druid : Character
+{
+	public Druid(): base ("Druid", 80, 130)
+	{
+		PhysDamageMultiplikator = 0.4;
+		MagicDamageMultiplikator = 1.2; 
+
+		PhysResistanceMultiplikator = 0.6;
+		MagicResistanceMultiplikator = 1.1;
 	}
 }
