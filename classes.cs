d5eac888ef6e70590e645dc69bc41ab6bb5eb16a
Perrond <udajerodzica2137@gmail.com>
2026-08-23 13:30:54 +0200

    classes.cs now does not give errors

--- classes.cs
+++ classes.cs
@@ -1,11 +1,7 @@
-# Wizard, Alchemist - Attributes: great attacking spells, making potions
-# Angels, Priest, Palladin - Attributes: healing, flying (over water)
-# Assasins, Bandits, Thiefs - Attributes: stealing gold, sneaky attack
-# Rouge - Attributes: sneaky attack, stealing gold, poison
-# Archers, cowboy - Attributes: Long ranged physical attack
-# Necromancers - Attributes: Long ranged magic attack, spawning undeads
-# 
-# 
-# 
-# 
-# 
+//# Wizard, Alchemist - Attributes: great attacking spells, making potions
+//# Angels, Priest, Palladin - Attributes: healing, flying (over water)
+//# Assasins, Bandits, Thiefs - Attributes: stealing gold, sneaky attack
+//# Rouge - Attributes: sneaky attack, stealing gold, poison
+//# Archers, cowboy - Attributes: Long ranged physical attack
+//# Necromancers - Attributes: Long ranged magic attack, spawning undeads
+//  # 
