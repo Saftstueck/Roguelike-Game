316c0763a6d12e4a7b517a514c1a253c3ab57e42
perrond <udajerodzica2137@gmail.com>
2026-08-23 13:54:46 +0200

    added items

---
+++ items.cs
@@ -0,0 +1,58 @@
+//--------------------Armor
+// 
+//----------Physical
+// Light Armor
+// medium Armor
+// Heavy Armor
+// Legendary Armor (Artefact)
+//----------Magic
+// Light Coat
+// Light Coat With a Hat
+// Medium Coat
+// Medium Coat With a Hat
+// Legendary Coat With a Hat (Artefact)
+//--------------------Items
+//
+//-------------Mele
+//
+//------Physical
+// Dagger
+// Sword
+// Great Sword
+// Battle Axe
+// Hammer (Artefact)
+//------Magic
+// Lightning Hammer (Artefact)
+// 
+// 
+// 
+// 
+//-------------Ranged
+//
+//------Physical
+// Bow
+// Crossbow
+// Great Bow
+// Great Crossbow
+// AK-47 (Artefact)
+//------Magic
+// Wooden Wand
+// Wooden Staff
+// Normal Staff
+// Great Staff
+// Cursed Staff (Artefact)
+//--------------------Consumables
+//
+//-------------Food
+// 
+// 
+// 
+// 
+// 
+//-------------Potions
+// Small Health Potion
+// Health Potion
+// Damage Potion
+// Poison Flask
+// Weakness Flask
+//__________________________________________________________________________THE END__________________________________________________________________________
