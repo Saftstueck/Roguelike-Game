7c8648d5084e2a8e729eefb0c1100233cd243aeb
Perrond <udajerodzica2137@gmail.com>
2026-09-04 20:57:32 +0200

    push

--- Area2d.cs
+++ Area2d.cs
@@ -1,15 +1,17 @@
 using Godot;
-using System;
 
 public partial class Area2d : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    public override void _Ready()
+    {
+        BodyEntered += OnBodyEntered;
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body is Player player)
+        {
+            player.Die();
+        }
+    }
 }
